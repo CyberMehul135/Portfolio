ca4f03828018b8b9549a5d9d9efffb989284bc73
Cyber Mehul <mehulrathod9254@gmail.com>
2025-03-10 16:21:09 +0530

    feat : add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <div className="w-full bg-transparent text-white px-14 absolute">
+      <div className="w-full max-w-[1600px] h-20 mx-auto flex justify-between items-center">
+        <span>
+          <img
+            src="/images/name-logo-white.svg"
+            alt="my icon"
+            className="w-16 h-16 hover:scale-110"
+          />
+        </span>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Navbar from "../components/Navbar";
+
 export default function Home() {
   return (
-    <>
-      <div>hi</div>
-    </>
+    <div className="">
+      <Navbar />
+      <section className="min-h-dvh text-white bg-[rgb(17,17,17)] pt-20">
+        k
+      </section>
+    </div>
   );
 }
