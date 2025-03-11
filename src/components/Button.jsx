73428704938e2860cd0b3ae9d6255cbd85fd1ac8
Cyber Mehul <mehulrathod9254@gmail.com>
2025-03-11 17:55:40 +0530

    refactor : optimize button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,11 @@
-export default function Button({ handleClick, text, element }) {
+export default function Button({ handleClick, text }) {
   return (
     <>
       <button
         onClick={handleClick}
-        className="text-white text-xl font-semibold max-w-[300px] bg-green-700 bg-gradient-to-r from-[rgb(29,41,99)] to-[rgb(21,146,180)] rounded mx-auto px-4 py-2 cursor-pointer hover:scale-110 transition-all duration-100 flex group"
+        className="text-white text-xl font-semibold max-w-[300px] bg-green-700 bg-gradient-to-r from-[rgb(29,41,99)] to-[rgb(21,146,180)] rounded mx-auto px-4 py-2 cursor-pointer hover:scale-110 transition-all duration-100 flex"
       >
         {text}
-        <span className="left-28 group-hover:left-32 transition-all duration-200 absolute">
-          {element}
-        </span>
       </button>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,15 @@ export default function Home() {
       <div className=" mx-auto flex flex-col  h-screen justify-between">
         <Navbar />
         <HeroSection />
-        <span className="mb-16 mx-auto">
+        <span className="mb-16 mx-auto relative group">
           <Button text={"Latest Works"} />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            className="w-5 fill-white absolute top-[120%] left-1/2 -translate-x-1/2 group-hover:top-[150%] transition-all duration-200"
+          >
+            <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+          </svg>
         </span>
       </div>
     </div>
