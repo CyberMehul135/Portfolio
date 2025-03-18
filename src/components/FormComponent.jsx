d23003b9a608b78518aad32044a873f01d69cdf3
Cyber Mehul <mehulrathod9254@gmail.com>
2025-03-19 01:08:36 +0530

    refactor : optimize FormComponent

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -3,7 +3,6 @@ import InputComponent from "./InputComponent";
 import TextAreaComponent from "./TextAreaComponent";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
-import { useState } from "react";
 
 export default function FormComponent({ darkMode }) {
   return (
@@ -17,11 +16,22 @@ export default function FormComponent({ darkMode }) {
       </div>
       <form
         className="w-1/2 flex flex-col gap-4 px-4 max-md:w-full max-sm:px-0"
-        name="contact"
+        action="https://formsubmit.co/mehulrathod9254@gmail.com"
         method="POST"
-        data-netlify="true"
       >
-        <input type="hidden" name="form-name" value="contact" />
+        {/* Hidden Inputs */}
+        <input
+          type="hidden"
+          name="_subject"
+          value="New Contact Form Submission!"
+        />
+        <input type="hidden" name="_captcha" value="false" />
+        <input type="hidden" name="_template" value="box" />
+        <input
+          type="hidden"
+          name="_next"
+          // value="https://yourdomain.com/thank-you"
+        />
         <InputComponent
           inputType={`text`}
           inputName={`name`}
