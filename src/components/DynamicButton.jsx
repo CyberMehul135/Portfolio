98845580cfe6ff6882af18996066e3d545fb115d
Cyber Mehul <mehulrathod9254@gmail.com>
2025-03-19 12:00:57 +0530

    refactor : optimize FormComponent for recieved responsed & send it to mail

--- src/components/DynamicButton.jsx
+++ src/components/DynamicButton.jsx
@@ -1,8 +1,10 @@
-export default function DynamicButton() {
+export default function DynamicButton({ success }) {
   return (
     <>
       <button
-        className="w-full h-10 bg-blue-500 text-white font-semibold rounded cursor-pointer flex justify-center items-center gap-2 hover:scale-105 transition-all duration-300"
+        className={`w-full h-10 text-white font-semibold rounded cursor-pointer flex justify-center items-center gap-2 hover:scale-105 transition-all duration-300 ${
+          success ? "bg-green-500" : "bg-blue-500"
+        } `}
         type="submit"
       >
         <div>
@@ -12,7 +14,7 @@ export default function DynamicButton() {
             alt="send-message-icon"
           />
         </div>
-        <div>Send Message</div>
+        <div>{success ? "Submited" : "Send Message"}</div>
       </button>
     </>
   );
--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,3 +1,60 @@
+// import DynamicButton from "./DynamicButton";
+// import InputComponent from "./InputComponent";
+// import TextAreaComponent from "./TextAreaComponent";
+// import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+// import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+
+// export default function FormComponent({ darkMode }) {
+//   return (
+//     <div className="flex justify-between py-10 max-md:flex-col">
+//       <div className="w-1/2 max-md:w-full">
+//         <img
+//           className="w-full h-full"
+//           src="/images/contact-me.svg"
+//           alt="contact-me-image"
+//         />
+//       </div>
+//       <form
+//         className="w-1/2 flex flex-col gap-4 px-4 max-md:w-full max-sm:px-0"
+//         action="https://formsubmit.co/mehulrathod9254@gmail.com"
+//         method="POST"
+//       >
+//         {/* Hidden Inputs */}
+//         <input
+//           type="hidden"
+//           name="_subject"
+//           value="New Contact Form Submission!"
+//         />
+//         <input type="hidden" name="_captcha" value="false" />
+//         <input type="hidden" name="_template" value="box" />
+//         <input
+//           type="hidden"
+//           name="_next"
+//           // value="https://yourdomain.com/thank-you"
+//         />
+//         <InputComponent
+//           inputType={`text`}
+//           inputName={`name`}
+//           inputLable={`Name`}
+//           inputIconUrl={<PermIdentityIcon />}
+//           darkMode={darkMode}
+//         />
+//         <InputComponent
+//           inputType={`email`}
+//           inputName={`email`}
+//           inputLable={`Email`}
+//           inputIconUrl={<AlternateEmailIcon />}
+//           darkMode={darkMode}
+//         />
+//         <TextAreaComponent darkMode={darkMode} />
+//         <DynamicButton />
+//       </form>
+//     </div>
+//   );
+// }
+
+import { useForm, ValidationError } from "@formspree/react";
+import { useEffect, useState } from "react";
 import DynamicButton from "./DynamicButton";
 import InputComponent from "./InputComponent";
 import TextAreaComponent from "./TextAreaComponent";
@@ -5,6 +62,21 @@ import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 
 export default function FormComponent({ darkMode }) {
+  const [state, handleSubmit] = useForm("myzekzad");
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  useEffect(() => {
+    if (state.succeeded) {
+      setShowSuccess(true); // success signal on
+
+      const timer = setTimeout(() => {
+        setShowSuccess(false); // success signal off after 3 sec
+      }, 3000);
+
+      return () => clearTimeout(timer); // cleanup
+    }
+  }, [state.succeeded]);
+
   return (
     <div className="flex justify-between py-10 max-md:flex-col">
       <div className="w-1/2 max-md:w-full">
@@ -14,40 +86,35 @@ export default function FormComponent({ darkMode }) {
           alt="contact-me-image"
         />
       </div>
+
       <form
+        onSubmit={handleSubmit}
         className="w-1/2 flex flex-col gap-4 px-4 max-md:w-full max-sm:px-0"
-        action="https://formsubmit.co/mehulrathod9254@gmail.com"
-        method="POST"
       >
-        {/* Hidden Inputs */}
-        <input
-          type="hidden"
-          name="_subject"
-          value="New Contact Form Submission!"
-        />
-        <input type="hidden" name="_captcha" value="false" />
-        <input type="hidden" name="_template" value="box" />
-        <input
-          type="hidden"
-          name="_next"
-          // value="https://yourdomain.com/thank-you"
-        />
         <InputComponent
-          inputType={`text`}
-          inputName={`name`}
-          inputLable={`Name`}
+          inputType="text"
+          inputName="name"
+          inputLable="Name"
           inputIconUrl={<PermIdentityIcon />}
           darkMode={darkMode}
         />
         <InputComponent
-          inputType={`email`}
-          inputName={`email`}
-          inputLable={`Email`}
+          inputType="email"
+          inputName="email"
+          inputLable="Email"
           inputIconUrl={<AlternateEmailIcon />}
           darkMode={darkMode}
         />
         <TextAreaComponent darkMode={darkMode} />
-        <DynamicButton />
+
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+
+        <DynamicButton disabled={state.submitting} success={showSuccess} />
       </form>
     </div>
   );
